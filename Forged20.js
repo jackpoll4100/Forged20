07082d74219178dea49f63c8b113ceccdd813850
jackpoll4100 <43215677+jackpoll4100@users.noreply.github.com>
2025-09-05 22:54:36 -0600

    New fancy macros, character sheet importer

--- Forged20.js
+++ Forged20.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Forged20
 // @namespace    jackpoll4100
-// @version      1.2
+// @version      1.3
 // @description  Allows rolling from forge steel character sheets into roll20.
 // @author       jackpoll4100
 // @match        https://andyaiken.github.io/forgesteel*
@@ -39,13 +39,56 @@
           document.querySelectorAll('[title="Text Chat Input"]')[0].value = macro;
           document.getElementById('chatSendBtn').click();
       }
+      function execImport(jsonCharacter)
+      {
+          if(document.querySelector('[name="attr_forgesteel_json"]'))
+          {
+              document.querySelector('[name="attr_forgesteel_json"]').value = jsonCharacter;
+              setTimeout(()=>
+              {
+                  document?.querySelector('[name="act_import"]')?.click();
+              }, 200);
+          }
+      }
       GM_onMessage('forgesteel-pipe', function(message) {
           console.log('forgesteel message received: ', message);
-          if (message.includes('template')){
+          if (message.includes('charactersheet---'))
+          {
+              if(window.location.href.includes('/character'))
+              {
+                  let cleanedString = message.split('charactersheet---')[1];
+                  execImport(cleanedString);
+              }
+          }
+          else if (message.includes('template')){
               let cleanedString = message.split('---')[1];
               execMacro(cleanedString);
           }
       });
+      if (window.location.href.includes('/character'))
+      {
+          const bodyTarget = document.querySelector('body');
+          const config = { attributes: false, childList: true, subtree: true };
+          function GM_sendMessage(label){
+              GM_setValue(label, Array.from(arguments).slice(1));
+          }
+          console.log('sending open message');
+          GM_sendMessage('roll20-pipe', 'roll20 opened');
+          const listenerSetup = ()=>{
+              const foundElement = document?.querySelector('[name="act_import"]');
+              if (foundElement && !foundElement.getAttribute('listener-applied')){
+                  foundElement.setAttribute('listener-applied', true);
+                  foundElement.parentElement.innerHTML += '<button type="button" id="f20-import">Import directly from Forge Steel</button>';
+                  document.getElementById('f20-import')?.addEventListener?.('click', ()=>
+                  {
+                      console.log(`requesting character from forgesteel`);
+                      GM_sendMessage(`roll20-pipe`, `${ Math.random() }---character please`);
+                  });
+              }
+          };
+          const observer = new MutationObserver(listenerSetup);
+          observer.observe(bodyTarget, config);
+      }
       function appendForgeSteelSettings(){
           let uiContainer = document.createElement('div');
           uiContainer.innerHTML = forgesteelSettingsTemplate;
@@ -74,6 +117,16 @@
       let classMap = {
           rollSelector: '.total > div.ant-statistic-content > span > span',
           rollTitles: ['.roll-modal div.ant-statistic-title', '.modal-content .ability-panel > div.header-text-panel > div > div.header-text'],
+          abilityTypeSelector: '.ability-modal .ability-info-panel > .ds-text',
+          rollModifierSelector: '.ability-modal .roll-state-selector .ant-segmented-item-selected > .ant-segmented-item-label',
+          pointSelector: '.ability-modal .header-text-content .pill',
+          keywordsSelector: '.ability-modal .ability-panel .ant-tag',
+          distanceTargetSelector: '.ability-modal .ability-info-panel > .field > .field-value',
+          rawDiceValuesSelector: '.ability-modal .result-row .ant-statistic-content-value-int',
+          effectDescriptionsSelector: '.ability-modal .ability-panel > div > div:not(.power-roll-panel) > p',
+          specialEffectDescriptionsSelector: '.ability-modal .ability-panel > div > div.field.horizontal',
+          flavorSelector: '.ability-modal .ability-description-text > p',
+          tiersSelector: '.ability-modal .ability-panel div.power-roll-panel p',
           rollButton: '.die-roll-panel > .ant-btn',
           effectsSelector: '.ant-drawer .power-roll-row .effect',
           criticalSuccess: '.ant-alert-success',
@@ -102,17 +155,34 @@
           };
       }
 
+      function GM_onMessage(label, callback){
+          GM_addValueChangeListener(label, function(){
+              callback.apply(undefined, arguments[2]);
+          });
+      }
+      GM_onMessage('roll20-pipe', function(message) {
+          console.log('roll20 message received: ', message);
+          if (message?.split?.('---')?.[1] === 'character please')
+          {
+              fetchCharacter((character)=>
+              {
+                  const characterJSON = JSON.stringify(character);
+                  console.log('Sending character to roll20: ', characterJSON);
+                  GM_sendMessage('forgesteel-pipe', `${ Math.random() }---charactersheet---` + characterJSON);
+              });
+          }
+      });
+
       function rollWatcher(){
           fetchCharacter((character) => {
               const roll = document.querySelector(classMap.rollSelector)?.innerHTML;
-
+              const rawRoll = parseInt(document.querySelectorAll(classMap.rawDiceValuesSelector)?.[0]?.innerHTML || 0) + parseInt(document.querySelectorAll(classMap.rawDiceValuesSelector)?.[1]?.innerHTML || 0);
               let rollTitle = '';
               classMap.rollTitles.forEach((selector)=>{
                   rollTitle = document.querySelector(selector)?.innerHTML ? document.querySelector(selector).innerHTML : rollTitle;
               });
-
-              const tierAlert = document.querySelector(classMap.tierAlert);
               let modifierText = '';
+              const tierAlert = document.querySelector(classMap.tierAlert);
               let rollTier = roll < 12 ? 1 : roll < 17 ? 2 : 3;
               if (tierAlert?.innerHTML?.includes('down') && rollTier > 1){
                   rollTier --;
@@ -123,12 +193,104 @@
                   modifierText = '(Tier was increased by a Double Edge)';
               }
 
-              const effects = document.querySelectorAll(classMap.effectsSelector);
-              const constructedEffect = effects.length === 3 ? `{{effect=${ effects[rollTier - 1].innerHTML } ${ modifierText }}}` : '';
+              const rollMode = 'new';
+              if(rollMode === 'legacy' || document.querySelector(classMap.rollTitles[0])?.innerHTML)
+              {
+                  // "Legacy" macro construction.
 
-              const constructedMessage = `&{template:default} {{name=${ character?.name ? `${ character.name }` : '' }}} ${ rollTitle ? `{{type=${ rollTitle }}}` : '' } {{result=${ roll } ${ document.querySelector(classMap.criticalSuccess) ? '(Critical Success)' : ''}}} ${ constructedEffect }`;
-              console.log('Sending message to roll20: ', constructedMessage);
-              GM_sendMessage('forgesteel-pipe', `${ Math.random() }---` + constructedMessage);
+                  const effects = document.querySelectorAll(classMap.effectsSelector);
+                  const constructedEffect = effects.length === 3 ? `{{effect=${ effects[rollTier - 1].innerHTML } ${ modifierText }}}` : '';
+
+                  const constructedMessage = `&{template:default} {{name=${ character?.name ? `${ character.name }` : '' }}} ${ rollTitle ? `{{type=${ rollTitle }}}` : '' } {{result=${ roll } ${ document.querySelector(classMap.criticalSuccess) ? '(Critical Success)' : ''}}} ${ constructedEffect }`;
+                  console.log('Sending message to roll20: ', constructedMessage);
+                  GM_sendMessage('forgesteel-pipe', `${ Math.random() }---` + constructedMessage);
+                  return;
+              }
+
+              let macroTemplate = `&{template:default} {{Resources Required=RESOURCES}} {{Keywords=KEYWORDS}} {{Type=ABILITYTYPE}} {{Distance=DISTANCE}} {{Target=TARGET}} {{Power Roll=[POWERROLL]POWERCSS}} {{Tier 1=TIER1}} {{Tier 2=TIER2}} {{Tier 3=TIER3}}{{name=[NAME](NAMECSS display: inline;)%NEWLINE%%NEWLINE%[FLAVOR](FLAVORCSS display: inline;)}} {{Effect=EFFECT}} {{SPECIALEFFECTS}} {{Edges and Banes=MODIFIER}}`;
+
+              // Apply Special Effects
+              const specialEffects = document.querySelectorAll(classMap.specialEffectDescriptionsSelector);
+              let specialEffectsString = '';
+              specialEffects.forEach(e => {
+                  const seName = e.querySelector('.field-label > div')?.innerHTML?.includes('<') ? e.querySelector('.field-label > div')?.innerHTML?.split?.('<')?.[0] : e.querySelector('.field-label')?.innerHTML;
+                  const sePoints = e.querySelector('.field-label .pill')?.innerHTML;
+                  const seBody = e.querySelector('.field-value p')?.innerHTML;
+                  specialEffectsString += `{{${ seName + (sePoints ? ` (${ sePoints?.replaceAll(' ', '') })` : '') }=${ seBody }}}`;
+              });
+              macroTemplate = macroTemplate.replaceAll('{{SPECIALEFFECTS}}', specialEffectsString);
+
+              // Apply Tier Upgrades
+              if (rawRoll < 19)
+              {
+                  macroTemplate = macroTemplate.replaceAll(`TIER${ rollTier }`, `[>>]POWERCSS [TIER${ rollTier }](HEADERCSS display: inline)[<<]POWERCSS`);
+              }
+              else
+              {
+                  macroTemplate = macroTemplate.replaceAll(`TIER${ rollTier }`, `[>>]POWERCSS [Critical Hit!]POWERCSS [TIER${ rollTier }](HEADERCSS display: inline)[<<]POWERCSS`);
+                  macroTemplate += '{{Extra=[Take Another Action!]CRITCSS}}';
+              }
+
+              // Apply inline styles
+              const powercss = '(" style=" color: yellow; font-weight: bold; text-shadow: 2px 2px black, -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; font-size: 15px; cursor: text;)';
+              const headercss = '" style="text-decoration: none; background: none; padding: 0px; font-size: 13px; cursor: text; display:none; border: none; text-shadow: 2px 2px black, -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; color: Yellow; font-weight: bold; white-space: wrap;';
+              const namecss = '" style="text-decoration: none; background: none; padding: 0px; font-size: 13px; font-weight: bold; cursor: text; display:none; border: none; text-shadow: 2px 2px black, -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; color: white; cursor: text; font-style: bold; font-size: 20px;';
+              const flavorcss = '" style="text-decoration: none; background: none; padding: 0px; font-size: 13px; font-weight: bold; cursor: text; display:none; border: none; text-shadow: 2px 2px black, -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; color: white; cursor: text; font-style: italic; font-size: 11px;';
+              const critcss = '(#" style=" font-weight: normal; display: block; color: yellow; font-weight: bold; text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black; cursor: text;)';
+              macroTemplate = macroTemplate.replaceAll('POWERCSS', powercss).replaceAll('HEADERCSS', headercss).replaceAll('NAMECSS', namecss).replaceAll('FLAVORCSS', flavorcss).replaceAll('CRITCSS', critcss);
+
+              // Apply Name
+              macroTemplate = macroTemplate.replaceAll('NAME', `${ character?.name ? `${ character.name }` : '' } - ${ rollTitle }`);
+
+              // Apply Power Roll
+              macroTemplate = macroTemplate.replaceAll('POWERROLL', roll);
+
+              // Apply Resources
+              const pointString = document.querySelector(classMap.pointSelector)?.innerHTML;
+              macroTemplate = macroTemplate.replaceAll('{{Resources Required=RESOURCES}}', (pointString && !pointString?.includes('Signature')) ? `{{Resources Required=${ pointString }}}` : '');
+
+              // Apply Distance and Target
+              const distanceTargetArr = document.querySelectorAll(classMap.distanceTargetSelector);
+              macroTemplate = macroTemplate.replaceAll('DISTANCE', distanceTargetArr?.[0]?.innerHTML || 'N/A');
+              macroTemplate = macroTemplate.replaceAll('TARGET', distanceTargetArr?.[1]?.innerHTML || distanceTargetArr?.[0]?.innerHTML || 'N/A');
+
+              // Apply Tiers
+              const tiersArr = document.querySelectorAll(classMap.tiersSelector);
+              macroTemplate = macroTemplate.replaceAll('TIER1', tiersArr?.[0]?.innerHTML || 'N/A');
+              macroTemplate = macroTemplate.replaceAll('TIER2', tiersArr?.[1]?.innerHTML || 'N/A');
+              macroTemplate = macroTemplate.replaceAll('TIER3', tiersArr?.[2]?.innerHTML || 'N/A');
+
+              // Apply Flavor
+              const flavor = document.querySelector(classMap.flavorSelector)?.innerHTML;
+              macroTemplate = macroTemplate.replaceAll('FLAVOR', flavor || 'N/A');
+
+              // Apply Type
+              const type = document.querySelector(classMap.abilityTypeSelector)?.innerHTML;
+              macroTemplate = macroTemplate.replaceAll('ABILITYTYPE', type || 'N/A');
+
+              // Apply Effects
+              const effects = document.querySelectorAll(classMap.effectDescriptionsSelector);
+              let effectsString = '';
+              effects.forEach(e => {
+                  effectsString += e.innerHTML + ' ';
+              });
+              macroTemplate = macroTemplate.replaceAll('{{Effect=EFFECT}}', effectsString ? `{{Effect=${effectsString}}}` : '');
+
+              // Apply Keywords
+              const keywords = document.querySelectorAll(classMap.keywordsSelector);
+              let keywordsString = '';
+              keywords.forEach(k => {
+                  keywordsString += k.innerHTML + ' ';
+              });
+              macroTemplate = macroTemplate.replaceAll('KEYWORDS', keywordsString || 'N/A');
+
+              // Apply Modifier
+              const modifier = document.querySelector(classMap.rollModifierSelector)?.innerHTML;
+              macroTemplate = macroTemplate.replaceAll('MODIFIER', (modifier && modifier !== 'Standard Roll') ? modifier : 'None' );
+
+              // Send to chat
+              console.log('Sending message to roll20: ', macroTemplate);
+              GM_sendMessage('forgesteel-pipe', `${ Math.random() }---` + macroTemplate);
           });
       }
 
